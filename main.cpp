4997abe112fe425a0e9f8317bedf45a859190baa
Skagge <staniszewskipatryk18@gmail.com>
2019-05-21 16:58:31 +0200

    akceptuj to

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main()
 	std::cout << "Hello World" << std::endl;
 	cout << "Mialem cos zmienic" << endl;
 	cout << "Aaaaaaa, nie rozumiem" << endl;
+	cout << "Kuba jest gruby" << endl;
 	return 1;
 }
