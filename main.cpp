ae4bcc06dd7209dcc03c86ff47780e0c2a4134c7
chesiak <ola.chesiak@gmail.com>
2019-05-21 17:12:15 +0200

    Revert "Update main.cpp"
    This reverts commit a9a31ee1da258373b1315dfd9cc02f55dd62efb0.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main()
 
 	int i = 0;
 	while ( i < 5 )  {
-		std::cout << "i = "<< i<< std::endl;
+		std::cout << "i = "<< i<< endl;
 		i++;
 	}
-	std::cout << "Welcome to GitHub" << std::endl;
+	std::cout << "Welcome to GitHub" << endl;
 	std::cout << "Hello Ola" << std::endl;
 
 	return 0;
